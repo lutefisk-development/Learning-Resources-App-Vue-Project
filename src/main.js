a17609562a9db5a4f0c3719ad7c7bab328324d3e
Per Kristian <kristiansvanberg@gmail.com>
2020-12-27 10:58:42 +0100

    Imported BaseCard as a global component, can be everywhere in the app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import BaseCard from './components/UI/BaseCard.vue';
 
-const app = createApp(App)
+const app = createApp(App);
+
+// Register global components, can be used everywhere in the app
+app.component('base-card', BaseCard);
+
+// Mount the app to the selector
 app.mount('#app');
