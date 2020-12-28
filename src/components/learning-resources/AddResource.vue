533e53894f10417f7b3f239451c7b1ee6786d398
Per Kristian <kristiansvanberg@gmail.com>
2020-12-28 02:09:24 +0100

    Updated AddResource component. Using BaseDialog component for validating that user input is valid, is user tries to submit the form without filling in all input fields the modal will trigger.

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>Please check all input fields.</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay!</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -23,13 +32,27 @@
 <script>
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDesc = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
 
+      if(enteredUrl.trim() === '' || enteredDesc.trim() === '' || enteredUrl.trim() === '') {
+        this.inputIsInvalid = true;
+        // Exits submitData method
+        return;
+      }
+
       this.addResource(enteredTitle, enteredDesc, enteredUrl);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     }
   }
 }
